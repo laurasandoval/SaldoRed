09529a39d516930d570ae1eff6b375f8fb77b77d
Lau <hi@lau.work>
2019-06-14 00:24:01 -0400

    Corrección de errores.
    Había eliminado un plugin que no debía eliminar jajaja

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -1,15 +1,6 @@
 cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports = [
     {
-      "id": "cordova-plugin-inappbrowser.inappbrowser",
-      "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
-      "pluginId": "cordova-plugin-inappbrowser",
-      "clobbers": [
-        "cordova.InAppBrowser.open",
-        "window.open"
-      ]
-    },
-    {
       "id": "cordova-plugin-taptic-engine.TapticEngine",
       "file": "plugins/cordova-plugin-taptic-engine/www/TapticEngine.js",
       "pluginId": "cordova-plugin-taptic-engine",
@@ -24,12 +15,21 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "ThreeDeeTouch"
       ]
+    },
+    {
+      "id": "cordova-plugin-inappbrowser.inappbrowser",
+      "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+      "pluginId": "cordova-plugin-inappbrowser",
+      "clobbers": [
+        "cordova.InAppBrowser.open",
+        "window.open"
+      ]
     }
   ];
   module.exports.metadata = {
-    "cordova-plugin-inappbrowser": "3.0.0",
     "cordova-plugin-taptic-engine": "2.1.0",
     "cordova-plugin-whitelist": "1.3.3",
-    "cordova-plugin-3dtouch": "1.3.6"
+    "cordova-plugin-3dtouch": "1.3.6",
+    "cordova-plugin-inappbrowser": "3.0.0"
   };
 });
--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -15,11 +15,21 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       "clobbers": [
         "ThreeDeeTouch"
       ]
+    },
+    {
+      "id": "cordova-plugin-inappbrowser.inappbrowser",
+      "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+      "pluginId": "cordova-plugin-inappbrowser",
+      "clobbers": [
+        "cordova.InAppBrowser.open",
+        "window.open"
+      ]
     }
   ];
   module.exports.metadata = {
     "cordova-plugin-whitelist": "1.3.3",
     "cordova-plugin-taptic-engine": "2.1.0",
-    "cordova-plugin-3dtouch": "1.3.6"
+    "cordova-plugin-3dtouch": "1.3.6",
+    "cordova-plugin-inappbrowser": "3.0.0"
   };
 });
