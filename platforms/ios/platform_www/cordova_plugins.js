9173296a844034ed7c475eb1cedcf513e4c7d96d
Lau <hi@lau.work>
2019-06-13 23:43:27 -0400

    Añadiendo funcionalidad 3D Touch

--- platforms/ios/platform_www/cordova_plugins.js
+++ platforms/ios/platform_www/cordova_plugins.js
@@ -9,15 +9,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
       ]
     },
     {
-      "id": "cordova-plugin-inappbrowser.inappbrowser",
-      "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
-      "pluginId": "cordova-plugin-inappbrowser",
-      "clobbers": [
-        "cordova.InAppBrowser.open",
-        "window.open"
-      ]
-    },
-    {
       "id": "cordova-plugin-3dtouch.ThreeDeeTouch",
       "file": "plugins/cordova-plugin-3dtouch/www/ThreeDeeTouch.js",
       "pluginId": "cordova-plugin-3dtouch",
@@ -29,7 +20,6 @@ cordova.define('cordova/plugin_list', function(require, exports, module) {
   module.exports.metadata = {
     "cordova-plugin-whitelist": "1.3.3",
     "cordova-plugin-taptic-engine": "2.1.0",
-    "cordova-plugin-inappbrowser": "3.0.0",
     "cordova-plugin-3dtouch": "1.3.6"
   };
 });
