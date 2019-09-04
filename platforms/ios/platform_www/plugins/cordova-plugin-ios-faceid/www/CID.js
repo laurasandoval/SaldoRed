e94332bc3e9c0c7ff598730b2bf591bf85ee43ea
Lau <hi@lau.work>
2019-09-04 09:06:23 -0400

    Correcciones generales

---
+++ platforms/ios/platform_www/plugins/cordova-plugin-ios-faceid/www/CID.js
@@ -0,0 +1,13 @@
+cordova.define("cordova-plugin-ios-faceid.cordova-plugin-ios-faceid", function(require, exports, module) {
+var exec = require('cordova/exec');
+
+var CID = {
+	// Check for authentication with biometrics, support both face and touch identification.
+	// @reason	the reason why your app wants to use authentication with biometrics
+	checkAuth: function(reason, success, error) {
+		exec(success, error, 'CID', 'checkAuth', [reason]);
+	}
+};
+
+module.exports = CID;
+});
